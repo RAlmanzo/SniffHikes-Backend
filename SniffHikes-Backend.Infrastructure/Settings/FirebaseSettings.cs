d3291b4c78e3b4725b2003ef7bcb459bfa06e013
Almanzo Rosseel <ralmanzo@gmail.com>
2025-07-03 16:59:17 +0200

    Create new class firebasesettings

---
+++ SniffHikes-Backend.Infrastructure/Settings/FirebaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Infrastructure.Settings
+{
+    public class FirebaseSettings
+    {
+        public required string DatabaseUrl { get; set; }
+        public required string StorageUrl { get; set; }
+    }
+}
