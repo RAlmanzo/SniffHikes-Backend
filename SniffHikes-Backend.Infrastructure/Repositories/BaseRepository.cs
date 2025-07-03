457ad0eea78424e2e894e969d27d7b3c99b9a3f8
Almanzo Rosseel <ralmanzo@gmail.com>
2025-07-03 17:58:42 +0200

    Create baserepository and impliment ibaserepository

---
+++ SniffHikes-Backend.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,44 @@
+﻿using SniffHikes_Backend.Core.Entities;
+using SniffHikes_Backend.Core.Interfaces.Repositories;
+using SniffHikes_Backend.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        public BaseRepository(FirebaseDbContext context)
+        {
+            string collectionTarget = typeof(T).Name + "s";
+        }
+
+        public Task<T> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(T toCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T toUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
