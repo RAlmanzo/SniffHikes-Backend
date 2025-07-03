9e11f29fcab92b9f7d4ade6b9d346ba15493ef5e
Almanzo Rosseel <ralmanzo@gmail.com>
2025-07-03 17:21:38 +0200

    Create extension method addinfrastructure on iservicecollection

---
+++ SniffHikes-Backend.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SniffHikes_Backend.Infrastructure.Data;
+using SniffHikes_Backend.Infrastructure.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Infrastructure.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config) 
+        {
+            services.Configure<FirebaseSettings>(config.GetSection("Firebase"));
+            services.AddSingleton<FirebaseDbContext>();
+
+            return services;
+        }
+    }
+}
