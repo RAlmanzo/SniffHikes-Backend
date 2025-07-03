cd14d24968af9d3e9dfa0de5aacf10ae46f7250e
Almanzo Rosseel <ralmanzo@gmail.com>
2025-07-03 17:22:08 +0200

    Create firebasedbcontext

--- SniffHikes-Backend.Infrastructure/Data/FirebaseDbContext.cs
+++ SniffHikes-Backend.Infrastructure/Data/FirebaseDbContext.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Database;
+using Firebase.Storage;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic;
+using SniffHikes_Backend.Infrastructure.Settings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +15,13 @@ namespace SniffHikes_Backend.Infrastructure.Data
 {
     public class FirebaseDbContext
     {
+        public FirebaseClient Client { get; }
+        public FirebaseStorage Storage { get; }
 
+        public FirebaseDbContext(IOptions<FirebaseSettings> settings)
+        {
+            Client = new FirebaseClient(settings.Value.DatabaseUrl);
+            Storage = new FirebaseStorage(settings.Value.StorageUrl);
+        }
     }
 }
