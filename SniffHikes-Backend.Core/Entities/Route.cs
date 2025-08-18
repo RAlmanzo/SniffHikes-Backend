5a54faf94214ce410cb549edb9c974f32d3c187b
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-18 19:14:39 +0200

    Edit propertie changes for route entity

--- SniffHikes-Backend.Core/Entities/Route.cs
+++ SniffHikes-Backend.Core/Entities/Route.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SniffHikes_Backend.Core.Entities
 {
-    internal class Route
+    public class Route : BaseEntity
     {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string AddressId { get; set; }
+        public required ICollection<string> ImageIds { get; set; }
+        public DateTime DateCreated { get; set; }
+        public ICollection<string>? CommentIds { get; set; }
+        public required string UserId { get; set; }
     }
 }
