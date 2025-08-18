fcbfd05c81fe80dfde3e55ba773abb6beec59be8
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-18 19:07:48 +0200

    Create image entity

---
+++ SniffHikes-Backend.Core/Entities/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Core.Entities
+{
+    public class Image
+    {
+        public required string FileName { get; set; }
+        public int? RouteId { get; set; }
+        public int? EventId { get; set; }
+    }
+}
