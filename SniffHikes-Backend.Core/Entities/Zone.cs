6c04175f6802bd074f2985c507dfc7a948372f2c
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-18 19:13:09 +0200

    Create zone entity

---
+++ SniffHikes-Backend.Core/Entities/Zone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Core.Entities
+{
+    public class Zone : BaseEntity
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required IEnumerable<Image> Images { get; set; }
+        public required string AddressId { get; set; }
+        public ICollection<string>? CommentIds { get; set; }
+        public string? OrganizerId { get; set; }
+    }
+}
