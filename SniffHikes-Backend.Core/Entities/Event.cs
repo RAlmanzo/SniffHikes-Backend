268c966c1ebba3fb49364ab0cba580c782bf6954
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-18 19:06:47 +0200

    Create event entity

---
+++ SniffHikes-Backend.Core/Entities/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SniffHikes_Backend.Core.Entities
+{
+    public class Event : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public int AddressId { get; set; }
+        public ICollection<string> ImageIds { get; set; }
+        public DateTime DateEvent { get; set; }
+        public DateTime DateCreated { get; set; }
+        public ICollection<string> CommentIds { get; set; }
+        public string OrganizerId { get; set; }
+        public ICollection<string> AttendingUserIds { get; set; }
+    }
+}
