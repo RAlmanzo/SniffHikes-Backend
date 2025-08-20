40213d6f200cf5a5c3ffd2fdccbc792fdb4420d2
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-20 19:34:00 +0200

    Add tasks to get images by eventid zoneid routid and userid

--- SniffHikes-Backend.Core/Interfaces/Repositories/IImageRepository.cs
+++ SniffHikes-Backend.Core/Interfaces/Repositories/IImageRepository.cs
@@ -9,5 +9,9 @@ namespace SniffHikes_Backend.Core.Interfaces.Repositories
 {
     public interface IImageRepository : IBaseRepository<Image>
     {
+        Task<IEnumerable<Image>> GetImagesByEventIdAsync(string eventId);
+        Task<IEnumerable<Image>> GetImagesByRouteIdAsync(string routeId);
+        Task<IEnumerable<Image>> GetImagesByZoneIdAsync(string zoneId);
+        Task<IEnumerable<Image>> GetImagesByUserIdAsync(string userId);
     }
 }
