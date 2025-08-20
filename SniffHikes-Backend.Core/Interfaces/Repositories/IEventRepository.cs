b82c60c5c9484884eae436c5485b08da09727b2e
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-20 19:25:29 +0200

    Create new interface ieventrepository

---
+++ SniffHikes-Backend.Core/Interfaces/Repositories/IEventRepository.cs
@@ -0,0 +1,13 @@
+﻿using SniffHikes_Backend.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Core.Interfaces.Repositories
+{
+    public interface IEventRepository : IBaseRepository<Event>
+    {
+    }
+}
