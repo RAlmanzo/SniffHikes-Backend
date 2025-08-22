88c4c26a677e4ab28d612149ebcba24cfe19d9e4
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-22 19:31:00 +0200

    Create commentrepository and inherit from baserepository and icommentrepository

---
+++ SniffHikes-Backend.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using SniffHikes_Backend.Core.Entities;
+using SniffHikes_Backend.Core.Interfaces.Repositories;
+using SniffHikes_Backend.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Infrastructure.Repositories
+{
+    public class CommentRepository : BaseRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository(FirebaseDbContext context) : base(context)
+        {
+        }
+    }
+}
