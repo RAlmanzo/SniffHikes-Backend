cda8e2dc11a93c54bee23a3fc0ccea999703f89c
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-22 19:32:04 +0200

    Create zonerepository and inherit from baserepository and izonerepository

---
+++ SniffHikes-Backend.Infrastructure/Repositories/ZoneRepository.cs
@@ -0,0 +1,18 @@
+﻿using SniffHikes_Backend.Core.Entities;
+using SniffHikes_Backend.Core.Interfaces.Repositories;
+using SniffHikes_Backend.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Infrastructure.Repositories
+{
+    public class ZoneRepository : BaseRepository<Zone>, IZoneRepository
+    {
+        public ZoneRepository(FirebaseDbContext context) : base(context)
+        {
+        }
+    }
+}
