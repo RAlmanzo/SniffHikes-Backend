1259400ee55c39fd0ccef244e2580935e8c50edf
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-22 19:30:32 +0200

    Create imagerepository and inherit from baserepository and iimagerepository

---
+++ SniffHikes-Backend.Infrastructure/Repositories/ImageRepository.cs
@@ -0,0 +1,43 @@
+﻿using SniffHikes_Backend.Core.Entities;
+using SniffHikes_Backend.Core.Interfaces.Repositories;
+using SniffHikes_Backend.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Infrastructure.Repositories
+{
+    public class ImageRepository : BaseRepository<Image>, IImageRepository
+    {
+        public ImageRepository(FirebaseDbContext context) : base(context)
+        {
+        }
+
+        public Task<IEnumerable<Image>> GetImagesByDogIdAsync(string dogId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Image>> GetImagesByEventIdAsync(string eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Image>> GetImagesByRouteIdAsync(string routeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Image>> GetImagesByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Image>> GetImagesByZoneIdAsync(string zoneId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
