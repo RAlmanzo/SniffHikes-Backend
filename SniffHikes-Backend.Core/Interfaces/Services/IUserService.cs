9539e8a0499d6abb117dbeed2a327891bf6f6b6a
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-26 19:15:47 +0200

    Create iuserservice and add tasks

---
+++ SniffHikes-Backend.Core/Interfaces/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using SniffHikes_Backend.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Core.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<User> GetUserByIdAsync(string id);
+        Task<List<User>> GetAllUsersAsync();
+        Task<bool> CreateUserAsync(User newUser);
+        Task<bool> UpdateUserAsync(User toUpdate);
+        Task<string> DeleteUserAsync(string id);
+        Task<bool> AddDogAsync(string userId, Dog dog);
+    }
+}
