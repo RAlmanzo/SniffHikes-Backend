c1a38621a9a446de7f7a867ce0a28adbfbbb89b9
Almanzo Rosseel <ralmanzo@gmail.com>
2025-08-26 19:16:07 +0200

    Create izoneservice and add tasks

---
+++ SniffHikes-Backend.Core/Interfaces/Services/IZoneService.cs
@@ -0,0 +1,21 @@
+﻿using SniffHikes_Backend.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Core.Interfaces.Services
+{
+    public interface IZoneService
+    {
+        Task<Zone> GetZoneByIdAsync(string id);
+        Task<List<Zone>> GetAllZonesAsync();
+        Task<string> CreateZoneAsync(Zone newZone);
+        Task<bool> UpdateZoneAsync(Zone toUpdate);
+        Task<string> DeleteZoneAsync(string id);
+        Task<bool> AddZoneCommentAsync(string id, Comment comment);
+        Task<bool> DeleteZoneCommentAsync(string id, string commentId);
+        Task<List<Zone>> SearchZoneByCity(string cityName);
+    }
+}
