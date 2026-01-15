f1ba110f47e0732721229861b71d43bfb0b20bae
Almanzo Rosseel <ralmanzo@gmail.com>
2026-01-15 18:11:10 +0100

    Add methods to add delete and update eventcomment

---
+++ SniffHikes-Backend.Core/Interfaces/Services/ICommentService.cs
@@ -0,0 +1,16 @@
+﻿using SniffHikes_Backend.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SniffHikes_Backend.Core.Interfaces.Services
+{
+    public interface ICommentService
+    {
+        Task<bool> AddEventCommentAsync(Comment comment);
+        Task<bool> DeleteEventCommentAsync(string commentId);
+        Task<Comment> UpdateEventCommentAsync(Comment comment);
+    }
+}
