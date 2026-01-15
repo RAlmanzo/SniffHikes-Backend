28c949905776829635006b3cf8fdd60870152f04
Almanzo Rosseel <ralmanzo@gmail.com>
2026-01-15 18:16:22 +0100

    Add methods to add delete and update zonecomment

--- SniffHikes-Backend.Core/Interfaces/Services/ICommentService.cs
+++ SniffHikes-Backend.Core/Interfaces/Services/ICommentService.cs
@@ -15,5 +15,8 @@ namespace SniffHikes_Backend.Core.Interfaces.Services
         Task<bool> AddRouteCommentAsync(Comment comment);
         Task<bool> DeleteRouteCommentAsync(string commentId);
         Task<Comment> UpdateRouteCommentAsync(Comment comment);
+        Task<bool> AddZoneCommentAsync(Comment comment);
+        Task<bool> DeleteZoneCommentAsync(string commentId);
+        Task<Comment> UpdateZoneCommentAsync(Comment comment);
     }
 }
