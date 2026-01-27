621c05b63c08da392738fa1f9cf537794ac76293
Almanzo Rosseel <ralmanzo@gmail.com>
2026-01-27 16:51:01 +0100

    Implement interface iuserservice into userservice class

--- SniffHikes-Backend.Core/Services/UserService.cs
+++ SniffHikes-Backend.Core/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SniffHikes_Backend.Core.Entities;
+using SniffHikes_Backend.Core.Interfaces.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace SniffHikes_Backend.Core.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        public Task<bool> AddDogAsync(string userId, Dog dog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateUserAsync(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeleteUserAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateUserAsync(User toUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
