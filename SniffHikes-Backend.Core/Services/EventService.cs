ebbc2475d01478e9c0d39215c1b761960c29982f
Almanzo Rosseel <ralmanzo@gmail.com>
2026-01-27 16:50:29 +0100

    Implement interface ieventservice into eventservice class

--- SniffHikes-Backend.Core/Services/EventService.cs
+++ SniffHikes-Backend.Core/Services/EventService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SniffHikes_Backend.Core.Entities;
+using SniffHikes_Backend.Core.Interfaces.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,51 @@ using System.Threading.Tasks;
 
 namespace SniffHikes_Backend.Core.Services
 {
-    public class EventService
+    public class EventService : IEventService
     {
+        public Task<string> CreateEventAsync(Event newEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeleteEventAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event>> GetAllEventsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event>> GetAllEventsByUserId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> GetEventByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event>> GetRegisteredEventsByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Event>> SearchEventByCity(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SignUpToEvent(string id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateEventAsync(Event toUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
